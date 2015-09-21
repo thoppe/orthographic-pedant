f69d40fb8b0317d95780b7b0400a0c5d08c6e312
thoppe <travis.hoppe@gmail.com>
2015-09-21 00:27:37 -0400

    Sleep function now maximizes download time

--- search_readme.py
+++ search_readme.py
@@ -4,7 +4,8 @@ import os
 shell_token  = "GITHUB_ORTHOGRAPHIC_TOKEN"
 GITHUB_TOKEN = os.environ[shell_token]
 
-import requests, os, json, codecs, time
+import requests, os, json, codecs, time, pprint
+from datetime import datetime
 
 # github's search API
 url = "https://api.github.com/search/repositories"
@@ -38,10 +39,19 @@ for word in WORDS:
     print word, js["total_count"]
     
     limit = int(r.headers["x-ratelimit-remaining"])
-    print "x-remaining-remaining", limit
+    #print "x-remaining-remaining", limit
+    #pprint.pprint(dict(r.headers))
+
+    utc_reset_time = int(r.headers["x-ratelimit-reset"])
+    reset_time = datetime.utcfromtimestamp(utc_reset_time)
     
     if limit < 2:
-        print "Sleeping for API limit."
-        time.sleep(60)
-        
-    time.sleep(1)
+
+
+        now_time = datetime.utcnow()
+        delta_seconds = (reset_time - now_time).total_seconds()
+
+        print "Sleeping {:0.3f} seconds for API limit.".format(delta_seconds)
+        time.sleep(delta_seconds)
+
+
